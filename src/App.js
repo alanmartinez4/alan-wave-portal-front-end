c0572016759e5709b6e47282221acfc344f0d06a
Alan Martinez <18110522+alanmartinez4@users.noreply.github.com>
2021-09-29 23:38:37 -0500

    Update App.js
    See if we can access the users account

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
-
 const App = () => {
-  const checkIfWalletIsConnected = () => {
     /*
-    * First make we we have access to window.ethereum
+    * Just a state variable we use to store our user's public wallet
     */
-    const { ethereum } = window;
+    const[currentAccount, setCurrentAccount] = useState("");
 
-    if(!ethereum){
-      console.log("Make sure you have metamask!");
-      return;
-    } else{
-      console.log("We have an ethereum object", ethereum)
-    }
+    const checkIfWalletIsConnected = async () => {
+      try {
+        const { ethereum } = window;
+
+        if(!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+        } else {
+          console.log("We have an ethereum object", ethereum)
+        }
+        
+        /*
+        * Check if we're authorized to access the user's wallet
+        */
+        const accounts = await ethereum.request({ method: 'eth_accounts'});
+
+        if(accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized accounts", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("No authorized account found.");
+        }
+      } catch (error){
+        console.log(error);
+      }
   }
 
 /*
