7b85eadb9265fdf1fbcac426b3d9c1a040f467fd
Alan Martinez <18110522+alanmartinez4@users.noreply.github.com>
2021-09-30 23:58:02 -0500

    Update App.js
    call the deployed smart contract from our web app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/WavePortal.json';
 
 const App = () => {
     /*
     * Just a state variable we use to store our user's public wallet
     */
     const[currentAccount, setCurrentAccount] = useState("");
+    const [totalWaves, setTotalWaves] = React.useState("");
+    
+    /*
+    * Create a variable here that holds the contract address affter you deploy
+    */
+    const contractAddress = "0x3aEA2454539Bf00D1C71D09d10C2C4B55049FBE3";
+
+    /*
+    * Create a variable here that references the abi content
+    */
+    const contractABI = abi.abi;
 
     const checkIfWalletIsConnected = async () => {
       try {
@@ -57,6 +69,42 @@ const connectWallet = async () => {
   }
 }
 
+const wave = async () => {
+  try {
+    const { ethereum } = window;
+
+    if(ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum); // ethers is a library that helps our frontend talk to our contract.
+      const signer = provider.getSigner(); // a provider is what we use to actually talk to Ethereum nodes.
+
+      // You're using contractABI here
+      const waveportalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+      let count = await waveportalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+
+      // update totalWaves to display
+      setTotalWaves(count.toNumber())
+
+      const waveTxn = await waveportalContract.wave();
+      console.log("Mining...", waveTxn.hash);
+
+      await waveTxn.wait();
+      console.log("Mined -- ", waveTxn.hash);
+
+      count = await waveportalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+
+      setTotalWaves(count.toNumber())
+    } else {
+        console.log("Ethereum object doesn't exist!");
+    } 
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
 /*
 * This runs our function when the page loads.
 */
@@ -75,7 +123,12 @@ const connectWallet = async () => {
         <div className="bio">
         I am alan and I am learning how to create and deploy a smart contract to an Ethereum network, pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
-        <button className="waveButton" onClick={null}>
+
+        <div className="bio">
+          Total Number of Waves is:  {totalWaves}
+        </div>
+
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
       {/*
